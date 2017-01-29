3d206784eda7ae11bcf437b07552803b3935a839
davidvujic <david@vujic.se>
2017-01-29 23:01:56 +0100

    add open-closed Bad code example

---
+++ examples/open-closed-principle-BAD.js
@@ -0,0 +1,29 @@
+class AjaxAdapter {
+  constructor() {
+    this.name = 'ajaxAdapter';
+  }
+}
+
+class NodeAdapter {
+  constructor() {
+    this.name = 'nodeAdapter';
+  }
+}
+
+class HttpRequester {
+  constructor(adapter) {
+    this.adapter = adapter;
+  }
+
+  fetch(url) {
+    if (this.adapter.name === 'ajaxAdapter') {
+      return makeAjaxCall(url).then((response) => {
+        // transform response and return
+      });
+    } else if (this.adapter.name === 'nodeAdapter') {
+      return makeHttpCall(url).then((response) => {
+        // transform response and return
+      });
+    }
+  }
+}
